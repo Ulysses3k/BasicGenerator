bb4ec2795549aa2c746536808e143ddc6df5ba63
Costas <ceconomoujr@gmail.com>
2021-09-22 00:10:27 +0300

    implement PluginGraph, synthDSP and effectDSP classes

--- Source/PluginGraph.h
+++ Source/PluginGraph.h
@@ -9,3 +9,24 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+#include "DSP/synthDSP.h"
+#include "DSP/effectDSP.h"
+
+class PluginGraph
+{
+public:
+    void prepareToPlay(double sampleRate, int samplesPerBlock, int outputChannels);
+    void releaseResources();
+    void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples);
+
+    void updateSynthPararms(const float freq, const float gain, const float cutoff, const bool gate);
+    void updateEffectParams(const float delay, const float feedback);
+
+private:
+    synthDSP fSynth;
+    effectDSP fEffect;
+
+    bool isPrepared{ false };
+};
